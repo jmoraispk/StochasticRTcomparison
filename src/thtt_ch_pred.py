61a1a2eab201a197678e52e614ef3887f02e3306
joao <jmoraispk@gmail.com>
2025-10-11 17:07:21 -0700

    add robust path logic, GPU IDX, model names

--- src/thtt_ch_pred.py
+++ src/thtt_ch_pred.py
@@ -30,6 +30,15 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
+from pathlib import Path
+import sys
+
+try:
+    BASE = Path(__file__).parents[1]
+except NameError:
+    BASE = Path('.').resolve() / 'src'
+sys.path.append(str(BASE))
+
 # To create sequences and videos
 from thtt_ch_pred_utils import db, nmse, split_data
 
@@ -45,7 +54,7 @@ from nr_channel_predictor_wrapper import (
 )
 
 import pandas as pd
-from pathlib import Path
+
 
 NT = 2
 NR = 1
@@ -63,24 +72,25 @@ INTERP_FACTOR = 10  # final interpolated numbers of points
                      # = number of points between samples - 2 (endpoints)
 # Note: if samples are 1m apart, and we want 10cm between points, 
 #       set INTERP_FACTOR = 102. 1 m / (102 - 2) = 10cm
-
-ROOT = Path(__file__).parents[1]
-DATA_FOLDER = ROOT / f'../data/ch_pred_data_{N_SAMPLES//1000}k_{MAX_DOOPLER}hz_{L}steps'
+ROOT = BASE / '../'
+DATA_FOLDER = (ROOT / f'data/ch_pred_data_{N_SAMPLES//1000}k_{MAX_DOOPLER}hz_{L}steps').resolve()
 
 GPU_IDX = 0
 SEED = 42
 
-MODELS_FOLDER = ROOT /f'../saved_models/ch_pred_models_{MAX_DOOPLER}hz_{L}steps_INTERP_{INTERP_FACTOR}'
+MODELS_FOLDER = ROOT / f'saved_models/ch_pred_models_{MAX_DOOPLER}hz_{L}steps_INTERP_{INTERP_FACTOR}'
 os.makedirs(MODELS_FOLDER, exist_ok=True)
 
 models = ['TDL-A', 'CDL-C', 'UMa', f'asu_campus_3p5_10cm_interp_{INTERP_FACTOR}']
+model_names = ['TDL-A', 'CDL-C', 'UMa', 'ASU-1cm']
 
-L_IN = 10  # input sequence length
+L_IN = 20  # input sequence length
 L_OUT = 5  # output gap length
+
 
 #%% [PYTORCH ENVIRONMENT] Train models
 
-horizons = [1, 3, 5]#, 10, 20, 40]
+horizons = [1, 3, 5, 10, 20, 40]
 
 val_loss_per_horizon_gru = {model: [] for model in models}
 val_loss_per_horizon_gru_best = {model: [] for model in models}
@@ -105,7 +115,7 @@ for model in models:
 
         trained_model, tr_loss, val_loss, elapsed_time = \
             train(ch_pred_model, x_train, y_train, x_val, y_val, 
-                initial_learning_rate=1e-4, batch_size=64, num_epochs=300, 
+                initial_learning_rate=2e-4, batch_size=128, num_epochs=600, 
                 verbose=True, patience=60, patience_factor=1,
                 best_model_path=model_weights_file.replace('.pth', '_best.pth'),
                 device_idx=GPU_IDX)
@@ -197,12 +207,12 @@ results_matrix = compute_nmse_matrix(models, horizon=L_OUT, l_in=L_IN,
 
 # Plot confusion matrix (NMSE in dB inside the function)
 # plot_test_matrix(results_matrix, models)
-plot_test_matrix(results_matrix, ['TDL-A', 'CDL-C', 'UMa', 'ASU-1cm'])
+plot_test_matrix(results_matrix, model_names)
 
 #%% Fine tuning models & evaluating performance on target datasets
 
 # Fine-tuning configuration and utilities
-finetuned_models_folder = MODELS_FOLDER + '_finetuned'
+finetuned_models_folder = MODELS_FOLDER / '_finetuned'
 os.makedirs(finetuned_models_folder, exist_ok=True)
 
 def predict_batched(model, x: np.ndarray, batch_size: int = 128) -> np.ndarray:
@@ -259,7 +269,8 @@ def fine_tune_and_test(models_list: list[str], horizon: int, l_in: int,
                 verbose=True,
                 patience=patience,
                 patience_factor=patience_factor,
-                best_model_path=best_ft_model_path
+                best_model_path=best_ft_model_path,
+                device_idx=GPU_IDX
             )
 
             # Save fine-tuned model
@@ -288,6 +299,6 @@ ft_matrix = fine_tune_and_test(models, horizon=L_OUT, l_in=L_IN,
 eye_idxs = np.eye(ft_matrix.shape[0], dtype=bool)
 ft_matrix[eye_idxs] = results_matrix[eye_idxs]
 
-plot_test_matrix(ft_matrix, models)
+plot_test_matrix(ft_matrix, model_names)
 
 # %%
